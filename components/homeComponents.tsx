78ec8772ff54cdfc378e4881ebab069b5c998f3d
Yindeish <adeshinaadam03@gmail.com>
2025-01-05 12:36:27 +0100

    added images

--- components/homeComponents.tsx
+++ components/homeComponents.tsx
@@ -311,64 +311,7 @@ export function MonthStyle() {
 export function Services() {
   const router = useRouter();
 
-  // const items = [
-  //   {
-  //     img: "/images/home/services/alteration.png",
-  //     title: `STYLE FOR YIU`,
-  //     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, eum.`,
-  //   },
-  //   {
-  //     img: "/images/home/services/errands.png",
-  //     title: `HIGH QUALITY PRODUCT`,
-  //     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, dignissimos?`,
-  //   },
-  //   {
-  //     img: "/images/home/services/event.png",
-  //     title: `EXCLUSIVE COLLECTION`,
-  //     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, dignissimos?`,
-  //   },
-  //   // {
-  //   //   img: "/images/home/services/fashion.png",
-  //   //   title: `EXCLUSIVE COLLECTION`,
-  //   //   desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, dignissimos?`,
-  //   // },
-  //   // {
-  //   //   img: "/images/home/services/garment.png",
-  //   //   title: `EXCLUSIVE COLLECTION`,
-  //   //   desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, dignissimos?`,
-  //   // },
-  //   {
-  //     img: "/images/home/services/gathering_celebration.png",
-  //     title: `EXCLUSIVE COLLECTION`,
-  //     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, dignissimos?`,
-  //   },
-  //   // {
-  //   //   img: "/images/home/services/lifestyle_concierge.png",
-  //   //   title: `EXCLUSIVE COLLECTION`,
-  //   //   desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, dignissimos?`,
-  //   // },
-  //   // {
-  //   //   img: "/images/home/services/personal.png",
-  //   //   title: `EXCLUSIVE COLLECTION`,
-  //   //   desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, dignissimos?`,
-  //   // },
-  //   {
-  //     img: "/images/home/services/travel_jet.png",
-  //     title: `EXCLUSIVE COLLECTION`,
-  //     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, dignissimos?`,
-  //   },
-  //   // {
-  //   //   img: "/images/home/services/styling.png",
-  //   //   title: `EXCLUSIVE COLLECTION`,
-  //   //   desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, dignissimos?`,
-  //   // },
-  //   {
-  //     img: "/images/home/services/private_corperate.png",
-  //     title: `EXCLUSIVE COLLECTION`,
-  //     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, dignissimos?`,
-  //   },
-  // ];
-
+ 
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
   const handleMouseEnter = (index: number) => setHoveredIndex(index);
@@ -390,7 +333,7 @@ export function Services() {
         </div>
 
         {services.map(
-          ({ desc, galleryImages, heroImage, link, title }, index) => (
+          ({ desc, galleryImages, heroImage, link, title, overview, thumbnail }, index) => (
             <div
               className={`col-span-1 wmin_md:h-[75vh] wmax_md:h-[55vh] wmax_lg:mt-[20px] grid grid-cols-1 grid-rows-[4.5fr_5.5fr] relative overflow-hidden rounded-[24px]`}
               key={index}
@@ -398,19 +341,19 @@ export function Services() {
               <div className="w-full h-full overflow-hidden col-span-1 row-start-1 row-end-2">
                 <img
                   className="w-full h-ful max-h-full object-cover"
-                  src={heroImage}
+                  src={thumbnail}
                   alt=""
                 />
               </div>
 
-              <div className="w-full h-full col-span-1 row-start-2 row-end-3 bg-black flex flex-col justify-between py-[1.5em] px-[2em]">
+              <div className="w-full h-full col-span-1 row-start-2 row-end-3 bg-[#0D0D0D] flex flex-col justify-between py-[1.5em] px-[2em]">
                 <div className="w-full h-fit flex flex-col gap-[4px] justify-end relative">
                   <div className="text-[24px] font-bold text-white uppercase">
                     {title}
                   </div>
 
                   <div className="text-[20px] font-bold text-[#9C9C9C]">
-                    {desc?.title}
+                    {overview}
                   </div>
                 </div>
 
@@ -429,8 +372,7 @@ export function Services() {
       {/* Deskop */}
 
       {/* Mobile */}
-      <Container className="wmin_md:hidden grid wmin_lg:grid-cols-3 wmin_lg:gap-[32px] wmax_lg:grid-cols-1 wmax_lg:gap-[8px] wmin_lg:py-[5em] wmax_lg:py-[3em]">
-        <div className="col-span-full flex wmin_md:flex-row wmax_md:flex-col wmin_md:justify-between wmax_md:gap-[8px]">
+      <Container className="wmin_md:hidden grid wmin_lg:grid-cols-3 wmin_lg:gap-[32px] wmax_lg:grid-cols-1 wmax_lg:gap-[8px] wmin_lg:py-[5em] wmax_lg:py-[3em]">]        <div className="col-span-full flex wmin_md:flex-row wmax_md:flex-col wmin_md:justify-between wmax_md:gap-[8px]">
           <div className="col-span-1 h-fit wmin_md:text-[40px] wmax_md:text-[24px] font-black text-white">
             OUR SERVICES
           </div>
@@ -457,7 +399,7 @@ export function Services() {
                     />
                   </div>
 
-                  <div className="w-full h-full col-span-1 row-start-2 row-end-3 bg-black flex flex-col gap-[3em] py-[1em] px-[1.5em]">
+                  <div className="w-full h-full col-span-1 row-start-2 row-end-3 bg-[#0D0D0D] flex flex-col gap-[3em] py-[1em] px-[1.5em]">
                     <div className="w-full h-fit flex flex-col gap-[4px] justify-end relative">
                       <div className="text-[20px] font-bold text-white uppercase">
                         {title}
@@ -565,7 +507,7 @@ export function ClientsFeedback() {
           >
             {items.map(({ img, comments, name, rating }, index) => (
               <div
-                className="wmin_md:w-[529px] wmax_md:w-[calc(90vw-20px)] wmin_md:h-[448px] h-fit bg-black flex flex-col wmin_md:gap-[32px] wmax_md:gap-[20px] items-center justify-center wmin_md:p-[3em] wmax_md:p-[1.5em] rounded-[24px] shadow-l shadow-[#B6B6B633 wmin_md:mr-[28px] wmax_md:mr-[20px]"
+                className="wmin_md:w-[529px] wmax_md:w-[calc(90vw-20px)] wmin_md:h-[448px] h-fit bg-[#0D0D0D] flex flex-col wmin_md:gap-[32px] wmax_md:gap-[20px] items-center justify-center wmin_md:p-[3em] wmax_md:p-[1.5em] rounded-[24px] shadow-l shadow-[#B6B6B633 wmin_md:mr-[28px] wmax_md:mr-[20px]"
                 key={index}
               >
                 {/* <div className="col-span-1 h-full">
@@ -580,7 +522,7 @@ export function ClientsFeedback() {
                   >
                     <path
                       d="M35.9996 36C36.7952 36 37.5583 35.6839 38.1209 35.1213C38.6835 34.5587 38.9996 33.7956 38.9996 33V25.674C38.9996 24.8783 38.6835 24.1153 38.1209 23.5527C37.5583 22.9901 36.7952 22.674 35.9996 22.674H31.8356C31.8356 21.62 31.8976 20.566 32.0216 19.512C32.2076 18.396 32.5176 17.404 32.9516 16.536C33.3856 15.668 33.9446 14.985 34.6286 14.487C35.3086 13.929 36.1766 13.65 37.2326 13.65V9C35.4966 9 33.9766 9.372 32.6726 10.116C31.3776 10.8506 30.265 11.8679 29.4176 13.092C28.5644 14.4394 27.9362 15.9168 27.5576 17.466C27.1747 19.1752 26.9875 20.9225 26.9996 22.674V33C26.9996 33.7956 27.3156 34.5587 27.8783 35.1213C28.4409 35.6839 29.2039 36 29.9996 36H35.9996ZM17.9996 36C18.7952 36 19.5583 35.6839 20.1209 35.1213C20.6835 34.5587 20.9996 33.7956 20.9996 33V25.674C20.9996 24.8783 20.6835 24.1153 20.1209 23.5527C19.5583 22.9901 18.7952 22.674 17.9996 22.674H13.8356C13.8356 21.62 13.8976 20.566 14.0216 19.512C14.2096 18.396 14.5196 17.404 14.9516 16.536C15.3856 15.668 15.9446 14.985 16.6286 14.487C17.3086 13.929 18.1766 13.65 19.2326 13.65V9C17.4966 9 15.9766 9.372 14.6726 10.116C13.3776 10.8506 12.265 11.8679 11.4176 13.092C10.5644 14.4394 9.93619 15.9168 9.55757 17.466C9.17472 19.1752 8.98752 20.9225 8.99957 22.674V33C8.99957 33.7956 9.31564 34.5587 9.87825 35.1213C10.4409 35.6839 11.2039 36 11.9996 36H17.9996Z"
-                      fill="black"
+                      fill="white"
                     />
                   </svg>
                 </div>
@@ -610,7 +552,7 @@ export function ClientsFeedback() {
           <div className="w-fit h-fit flex gap-[20px]">
             {items.map(({ img, comments, name, rating }, index) => (
               <div
-                className="wmin_md:w-[529px] wmax_md:w-[calc(90vw-20px)] wmin_md:h-[448px] h-fit bg-black flex flex-col wmin_md:gap-[32px] wmax_md:gap-[20px] items-center justify-center wmin_md:p-[3em] wmax_md:p-[1.5em] rounded-[24px] shadow-l shadow-[#B6B6B633 wmin_md:mr-[28px] wmax_md:mr-[20px]"
+                className="wmin_md:w-[529px] wmax_md:w-[calc(90vw-20px)] wmin_md:h-[448px] h-fit bg-[#0D0D0D] flex flex-col wmin_md:gap-[32px] wmax_md:gap-[20px] items-center justify-center wmin_md:p-[3em] wmax_md:p-[1.5em] rounded-[24px] shadow-l shadow-[#B6B6B633 wmin_md:mr-[28px] wmax_md:mr-[20px]"
                 key={index}
               >
                 {/* <div className="col-span-1 h-full">
@@ -625,7 +567,7 @@ export function ClientsFeedback() {
                   >
                     <path
                       d="M35.9996 36C36.7952 36 37.5583 35.6839 38.1209 35.1213C38.6835 34.5587 38.9996 33.7956 38.9996 33V25.674C38.9996 24.8783 38.6835 24.1153 38.1209 23.5527C37.5583 22.9901 36.7952 22.674 35.9996 22.674H31.8356C31.8356 21.62 31.8976 20.566 32.0216 19.512C32.2076 18.396 32.5176 17.404 32.9516 16.536C33.3856 15.668 33.9446 14.985 34.6286 14.487C35.3086 13.929 36.1766 13.65 37.2326 13.65V9C35.4966 9 33.9766 9.372 32.6726 10.116C31.3776 10.8506 30.265 11.8679 29.4176 13.092C28.5644 14.4394 27.9362 15.9168 27.5576 17.466C27.1747 19.1752 26.9875 20.9225 26.9996 22.674V33C26.9996 33.7956 27.3156 34.5587 27.8783 35.1213C28.4409 35.6839 29.2039 36 29.9996 36H35.9996ZM17.9996 36C18.7952 36 19.5583 35.6839 20.1209 35.1213C20.6835 34.5587 20.9996 33.7956 20.9996 33V25.674C20.9996 24.8783 20.6835 24.1153 20.1209 23.5527C19.5583 22.9901 18.7952 22.674 17.9996 22.674H13.8356C13.8356 21.62 13.8976 20.566 14.0216 19.512C14.2096 18.396 14.5196 17.404 14.9516 16.536C15.3856 15.668 15.9446 14.985 16.6286 14.487C17.3086 13.929 18.1766 13.65 19.2326 13.65V9C17.4966 9 15.9766 9.372 14.6726 10.116C13.3776 10.8506 12.265 11.8679 11.4176 13.092C10.5644 14.4394 9.93619 15.9168 9.55757 17.466C9.17472 19.1752 8.98752 20.9225 8.99957 22.674V33C8.99957 33.7956 9.31564 34.5587 9.87825 35.1213C10.4409 35.6839 11.2039 36 11.9996 36H17.9996Z"
-                      fill="black"
+                      fill="white"
                     />
                   </svg>
                 </div>
