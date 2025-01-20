c9f7a8ca1146552a9c4d364db77be0c3a2c0eb9f
Yindeish <adeshinaadam03@gmail.com>
2025-01-20 07:13:54 +0100

    added all imgs respectively

--- app/about_us/page.tsx
+++ app/about_us/page.tsx
@@ -14,7 +14,7 @@ const About = () => {
 
       <Gallery />
 
-      <Partners />
+      {/* <Partners /> */}
 
       <ClientsFeedbackContactForm />
     </div>
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -4,13 +4,13 @@ import Container from "@/components/container";
 import { ContactForm, MonthStyle } from "@/components/homeComponents";
 import { useState } from "react";
 import { useSearchParams } from "next/navigation";
-import { IService, services } from "@/constants/services";
+import { IService, services, shaped_education } from "@/constants/services";
 
 const Service = () => {
   const searchParams = useSearchParams();
   const serviceId = searchParams.get("serviceId") as keyof typeof services;
 
-  const service = services.find(
+  const service = services.concat([shaped_education]).find(
     (service) => service.link === serviceId
   ) as IService;
 
@@ -37,7 +37,8 @@ export default Service;
 
 function Hero({ service }: { service: IService }) {
   return (
-    <div className="w-full wmin_390:h-[calc(100vh-80px)] wmax_360:!h-[65vh] wmin_360:wmax_md:!h-[70vh] bg-[#373737] wmax_lg:relative bg-[url(/images/fashion/hero_img.png)] bg-cover bg-no-repeat wmax_lg:bg-center">
+    // <div className="w-full wmin_390:h-[calc(100vh-80px)] wmax_360:!h-[65vh] wmin_360:wmax_md:!h-[70vh] bg-[#373737] wmax_lg:relative bg-[url(/images/fashion/hero_img.png)] bg-cover bg-no-repeat wmax_lg:bg-center">
+    <div className="w-full wmin_390:h-[calc(100vh-80px)] wmax_360:!h-[65vh] wmin_360:wmax_md:!h-[70vh] bg-[#373737] wmax_lg:relative bg-cover bg-no-repeat wmax_lg:bg-center" style={{backgroundImage: `url(${service?.heroImage})`}}>
       <div className="wmin_xl:w-full wmax_xl:w-[90%] h-fit flex flex-col items-center gap-[15px] absolute bottom-[4%] left-0 wmax_xl:left-[5%] z-[3]">
         <span className="text-white wmax_xl:text-[20px] wmin_xl:text-[48px] font-black text-center uppercase">
           {service?.title}
@@ -72,7 +73,7 @@ function Gallery({ service }: { service: IService }) {
   return (
     <div className="w-full h-fit bg-black">
       <Container className="w-full h-fit grid wmin_md:grid-cols-2 wmax_md:grid-cols-1 wmin_md:grid-rows-2">
-        {service.galleryImages.map((item, index) => (
+        {service?.galleryImages.map((item, index) => (
           <div
             className="col-span-1 wmin_390:h-[430px] wmax_390:h-[350px] relative"
             key={index}
