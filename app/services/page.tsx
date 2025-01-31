790be6fcf9e84acefca26a3ef431ff9217360e47
Yindeish <adeshinaadam03@gmail.com>
2025-01-31 17:07:50 +0100

    update Hero component overlay positioning for improved layout and responsiveness

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -66,7 +66,7 @@ function Hero({ service }: { service: IService }) {
       </div>
 
       {/* Overlay */}
-      <div className="w-full h-full bg-black bg-opacity-50"></div>
+      <div className="w-full h-full absolute top-0 left-0 z-[2] bg-black bg-opacity-50"></div>
       {/* Overlay */}
     </div>
   );
