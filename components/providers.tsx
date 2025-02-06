11696405b314534fe6e5926967ac18b6a7309b0a
Yindeish <adeshinaadam03@gmail.com>
2025-02-07 00:32:02 +0100

    fix: update meta tags for Open Graph and Twitter card in Providers component

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import "../app/globals.css";
 import Footer from "@/components/footer";
@@ -12,59 +12,57 @@ import { usePathname } from "next/navigation";
 import Head from "next/head";
 
 export default function Providers({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    const [loading, setLoading] = useState(true);
-    const path = usePathname()
+  const [loading, setLoading] = useState(true);
+  const path = usePathname();
 
-    useEffect(() => {
-        path != '/' && setLoading(false);
-    }, [path])
+  useEffect(() => {
+    path != "/" && setLoading(false);
+  }, [path]);
 
-    return (
-        <html lang="en">
-            {/* {loading ? (
+  return (
+    <html lang="en">
+      {/* {loading ? (
                 <body className={`w-full h-full ${path != '/' ? 'animate-pulse' : ''} bg-black`}>
                     {path == '/' && <Loader onFinish={() => setLoading(false)} />}
                 </body>
             ) :  */}
-            (
-                <body
-                    className={`w-full h-screen antialiased relative wmin_390:pt-[80px] wmax_390:pt-[65px] ${raleway.className}`}
-                >
-                     <Head>
-        {/* Basic Meta Tags */}
-        <title>Shaped Servics</title>
-        <meta name="description" content="Delivering bespoke fashion concierge services" />
-        <meta name="keywords" content="keyword1, keyword2, keyword3" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta charSet="UTF-8" />
+      (
+      <body
+        className={`w-full h-screen antialiased relative wmin_390:pt-[80px] wmax_390:pt-[65px] ${raleway.className}`}
+      >
+        <Head>
+          <title>Shaped Services</title>
+          <meta name="description" content="" />
 
-        {/* Open Graph Meta Tags */}
-        <meta property="og:title" content="Shaped Servics" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.shapedservices.com" />
-        {/* <meta property="og:image" content="/image.png" /> */}
-        <meta property="og:image" content="https://www.shapedservices.com/image.png" />
-        <meta property="og:description" content="Delivering bespoke fashion concierge services" />
+          <meta property="og:url" content="https://www.shapedservices.com" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="" />
+          <meta property="og:description" content="" />
+          <meta property="og:image" content="https://www.shapedservices.com/image.png" />
 
-        {/* Twitter Card Meta Tags */}
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content="Shaped Servics" />
-        <meta name="twitter:description" content="Delivering bespoke fashion concierge services" />
-        <meta property="og:image" content="https://www.shapedservices.com/image.png" />
-      </Head>
-                    <ModalProvider>
-                        <HeaderProvider>
-                            <Header className="bg-black wmax_390:h-[65px] wmin_390:h-[80px] fixed top-0 left-0 z-[40]" />
-                            {children}
-                            <Footer />
-                        </HeaderProvider>
-                    </ModalProvider>
-                </body>)
-                {/* } */}
-        </html>
-    );
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="shapedservices.com" />
+          <meta
+            property="twitter:url"
+            content="https://www.shapedservices.com"
+          />
+          <meta name="twitter:title" content="" />
+          <meta name="twitter:description" content="" />
+          <meta name="twitter:image" content="https://www.shapedservices.com/image.png" />
+        </Head>
+        <ModalProvider>
+          <HeaderProvider>
+            <Header className="bg-black wmax_390:h-[65px] wmin_390:h-[80px] fixed top-0 left-0 z-[40]" />
+            {children}
+            <Footer />
+          </HeaderProvider>
+        </ModalProvider>
+      </body>
+      ){/* } */}
+    </html>
+  );
 }
